49be06acc5d896b2ab74cc8d2b3bde97f8e71ec6
李华 <1554848817@qq.com>
2020-11-04 16:13:13 +0800

    多选框indeterminate不确定状态

--- src/views/form/components/checkbox/index.vue
+++ src/views/form/components/checkbox/index.vue
@@ -4,7 +4,6 @@
     <!-- 多选框 -->
     <div class="margin-b10px">
       <el-checkbox
-        :indeterminate="indeterminate"
         v-model="checked"
         true-label="1"
         false-label="2"
@@ -14,18 +13,38 @@
       <el-checkbox v-model="checked1" @change="change">梅西</el-checkbox>
     </div>
     <!-- 多选框组 -->
-    <el-checkbox
-      :indeterminate="indeterminate"
-      v-model="checked"
-      @change="change"
-      >全选</el-checkbox
-    >
+    <div>
+      <p>全选按钮</p>
+      <el-checkbox
+        :indeterminate="indeterminate"
+        v-model="isAll"
+        @change="handleSelectAll"
+        class="all"
+        >全选</el-checkbox
+      >
 
-    <el-checkbox-group v-model="checkList" @change="change">
-      <!-- el-checkbox label属性只有在使用多选框组的时候有用效 -->
-      <el-checkbox label="1" checked>C罗</el-checkbox>
-      <el-checkbox label="2">梅西</el-checkbox>
-    </el-checkbox-group>
+      <el-checkbox-group v-model="checkList" @change="handleSelectSingle">
+        <!-- el-checkbox label属性只有在使用多选框组的时候有用效 -->
+        <el-checkbox label="1" checked>C罗</el-checkbox>
+        <el-checkbox label="2">梅西</el-checkbox>
+      </el-checkbox-group>
+    </div>
+
+    <div>
+      <p>限制选择个数</p>
+      <el-checkbox-group
+        v-model="checkListNumber"
+        @change="change"
+        min="1"
+        max="3"
+      >
+        <!-- el-checkbox label属性只有在使用多选框组的时候有用效 -->
+        <el-checkbox label="1">C罗</el-checkbox>
+        <el-checkbox label="2">梅西</el-checkbox>
+        <el-checkbox label="3">罗本</el-checkbox>
+        <el-checkbox label="4">范佩西</el-checkbox>
+      </el-checkbox-group>
+    </div>
   </div>
 </template>
 
@@ -37,18 +56,36 @@ export default {
       checked1: true,
       indeterminate: true,
       checkList: [], //多选框组回显时，绑定数组里的值必须和多选框的label全等。
-    };
+      isAll: false, //标志是否全选
+      checkListNumber: [],
+    }
   },
   methods: {
     change(val) {
-      console.log(val);
-      this.indeterminate = !val;
+      console.log(val)
+      this.indeterminate = !val
+    },
+    // 点击全选按钮
+    handleSelectAll(val) {
+      console.log('val', val)
+
+      this.checkList = val ? ['1', '2'] : []
+      this.indeterminate = false
+    },
+    // 点击选中某一个
+    handleSelectSingle(val) {
+      let count = val.length
+      this.isAll = count === ['1', '2'].length
+      this.indeterminate = count > 0 && count < ['1', '2'].length
     },
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
 .checkbox {
+  .all {
+    margin: 0 79px 10px 0;
+  }
 }
 </style>
