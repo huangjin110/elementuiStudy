7214931fa509f0afc4e57f798c5b7dda1bb6bad3
黄进 <474505394@qq.com>
2020-11-09 22:12:13 +0800

    选择器

---
+++ src/views/form/components/select/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="select">
+    <!-- 选择器类型 1.单选，2.多选 select绑定的数据类型不一样，多选需要绑定数组-->
+    <!-- 1.基础用法，2.可搜索，3.自定义选项模板（分组），4.可创建条目 -->
+
+    <!-- 单选 -->
+    <h5>单选</h5>
+    <el-select v-model="value" placeholder="请选择" clearable @change="change">
+      <el-option
+        v-for="(item, index) in options"
+        :label="item.label"
+        :value="item.value"
+        :key="index"
+      >
+      </el-option>
+    </el-select>
+
+    <!-- 多选 -->
+    <h5>多选</h5>
+    <el-select
+      v-model="multiple"
+      placeholder="请选择"
+      multiple
+      clearable
+      @change="change"
+    >
+      <el-option
+        v-for="(item, index) in options"
+        :label="item.label"
+        :value="item.value"
+        :key="index"
+      >
+      </el-option>
+    </el-select>
+
+    <!-- 搜索 -->
+    <h5>搜索</h5>
+    <el-select
+      v-model="multiple"
+      placeholder="请选择"
+      multiple
+      clearable
+      filterable
+      @change="change"
+    >
+      <el-option
+        v-for="(item, index) in options"
+        :label="item.label"
+        :value="item.value"
+        :key="index"
+      >
+      </el-option>
+    </el-select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      multiple: [],
+      options: [
+        { label: "蛋糕", value: 1 },
+        { label: "双皮奶", value: 2 },
+        { label: "烧仙草", value: 3 },
+        { label: "果茶", value: 4 },
+      ],
+    };
+  },
+  methods: {
+    change(val) {
+      console.log(val);
+      console.log(this.multiple);
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -6,36 +6,37 @@
         :label="item.label"
         v-for="(item, index) in typeList"
         :key="item + index"
-      >{{item.name}}</el-radio-button>
+        >{{ item.name }}</el-radio-button
+      >
     </el-radio-group>
-    <component
-    :is="tabPosition"
-    />
+    <component :is="tabPosition" />
   </div>
 </template>
 
 <script>
-import radio from './components/radio/index'
-import checkbox from './components/checkbox/index'
-import myInput from './components/input/index'
-
+import radio from "./components/radio/index";
+import checkbox from "./components/checkbox/index";
+import myInput from "./components/input/index";
+import mySelect from "./components/select/index";
 
 export default {
-    components:{
-        radio,
-        checkbox,
-        myInput
-    },
-    data(){
-        return{
-            tabPosition:'radio',
-            typeList:[
-                {label:'radio',name:'单选'},
-                {label:'checkbox',name:'多选框'},
-                {label:'my-Input',name:'输入框'}
-            ]
-        }
-    }
+  components: {
+    radio,
+    checkbox,
+    myInput,
+    mySelect
+  },
+  data() {
+    return {
+      tabPosition: "radio",
+      typeList: [
+        { label: "radio", name: "单选" },
+        { label: "checkbox", name: "多选框" },
+        { label: "my-Input", name: "输入框" },
+        { label: "my-select", name: "选择器" },
+      ],
+    };
+  },
 };
 </script>
 
